e151bd36c96e0677f8f4a0d7bd74b03f9207287f
Behdad Esfahbod <behdad@behdad.org>
2023-09-21 15:13:23 -0600

    Filter out combining marks and empty glyphs

--- kern_language.py
+++ kern_language.py
@@ -2,10 +2,14 @@ import bigrams
 import kern
 import cairoft
 import functools
+import unicodedata
 
 @functools.cache
 def create_blurred_surface_for_text(text):
-    return kern.blur(kern.create_surface_for_text(text), kern.KERNEL)
+    surface = kern.create_surface_for_text(text)
+    if kern.surface_sum(surface) == 0:
+        return None
+    return kern.blur(surface, kern.KERNEL)
 
 if __name__ == '__main__':
     import sys
@@ -20,8 +24,15 @@ if __name__ == '__main__':
 
     all_bigrams = bigrams.extract_bigrams(txtfile, frqfile)
     for bigram in all_bigrams:
+
+        if unicodedata.category(bigram[0]) == 'Mn' or unicodedata.category(bigram[1]) == 'Mn':
+            continue
+
         l = create_blurred_surface_for_text(bigram[0])
         r = create_blurred_surface_for_text(bigram[1])
+
+        if l is None or r is None:
+            continue
 
         kern_value = kern.kern_pair(l, r, blurred=True)
         if kern_value == 0:
