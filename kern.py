f9ca0110821a181e28675650a1b6c1ffcdc7c5c6
Behdad Esfahbod <behdad@behdad.org>
2023-09-21 21:54:04 -0600

    Unintuitive

--- kern.py
+++ kern.py
@@ -118,7 +118,7 @@ def surface_sum(surface):
     s = 0
     for i in range(height):
         for j in range(width):
-            s += data[i * stride + j]
+            s += data[i * stride + j] ** .5
 
     return s
 
